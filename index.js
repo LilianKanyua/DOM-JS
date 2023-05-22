21e40f9ba5f26bbca5c3484785fb8f2f8e57b172
LilianKanyua <liliankanyua@gmail.com>
2023-05-22 19:38:59 +0300

    commit

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ document.getElementById("title").style.color="green";
 document.getElementById("title").style.textAlign ="center";
 document.getElementById("title").style.fontSize="40px";
 
-document.getElementById("heading_three").style.textTransform ="uppercase";
+document.getElementById("heading_fruits").style.textTransform ="uppercase";
+document.getElementById("heading_veg").style.textTransform ="uppercase";
 
 let anotherFruit= document.createElement("li");
 anotherFruit.innerHTML = "Apple";
